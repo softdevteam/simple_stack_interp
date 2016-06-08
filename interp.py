2bafaf62a1b0c194ba06169a61ca9b22ae80d0dd
Gabriela Alexandra Moldovan <gabi_250@live.com>
2016-06-08 14:35:04 +0100

    Added classes for different operations, changed variable dict

--- interp.py
+++ interp.py
@@ -20,88 +20,94 @@ jit_driver = jit.JitDriver (
 )
 
 class Op(object):
-
-    def __init__(self, name, args = None):
+    def __init__(self, name):
         self.name = name
-        if args is not None:
-            self.args = args
-        else:
-            self.args = []
 
     def __str__(self):
-        return "%s %s" % (self.name, self.args)
+        return self.name
+
+class StringOp(Op):
+    def __init__(self, name, args):
+        Op.__init__(self, name)
+        self.args = args
+
+class IntOp(Op):
+    def __init__(self, name, args):
+        Op.__init__(self, name)
+        self.args = int(args)
 
 class Interpreter(object):
-
     operations = ["ADD", "SUB", "MUL", "DIV", "MOD"]
 
     def __init__(self, prog):
         self.prog = prog
         self.pc = 0
         self.stack = []
-        self.vars = {}  # name -> val
+        self.vars = []
+        self.var_offsets = {}
 
     def dump_stack(self):
-        print("debug (pc=%s): %s" % (self.pc, self.stack))
+        print("debug (pc = %s): %s" % (self.pc, self.stack))
 
     def get_variable(self, name):
-        return self.vars.get(name, 0)
+        off = self.get_variable_off(name)
+        return self.vars[off]
+
+    @jit.elidable_promote()
+    def get_variable_off(self, name):
+        return self.var_offsets[name]
+
+    def set_variable(self, name, val):
+        off = self.var_offsets.get(name, -1)
+        if off == -1:
+            self.var_offsets[name] = len(self.vars)
+            self.vars.append(val)
+            return
+        self.vars[off] = val
 
     def run(self):
-
         while self.pc < len(self.prog):
-
             jit_driver.jit_merge_point(pc = self.pc, self = self)
-
-            #self.dump_stack()
             command = self.prog[self.pc]
-            #print command
-
             if command.name == "PUSH":
-                self.stack.append(int(command.args[0]))
-
+                assert isinstance(command, IntOp)
+                self.stack.append(command.args)
             elif command.name == "POP":
+                assert isinstance(command, Op)
                 self.stack.pop()
-
             elif command.name == "PRINT":
+                assert isinstance(command, Op)
                 print self.stack[-1]
-
             elif command.name == "STORE":
-                self.vars[command.args[0]] = self.stack.pop()
-
+                assert isinstance(command, StringOp)
+                self.set_variable(command.args, self.stack.pop())
             elif command.name == "LOAD":
-                self.stack.append(self.get_variable(command.args[0]))
-
+                assert isinstance(command, StringOp)
+                self.stack.append(self.get_variable(command.args))
             elif command.name == "JMP":
-                self.pc = int(command.args[0])
+                assert isinstance(command, IntOp)
+                self.pc = command.args
                 continue
-
             elif command.name == "CJMP":
+                assert isinstance(command, IntOp)
                 x = self.stack.pop()
                 if x == 1:
-                    self.pc = int(command.args[0])
+                    self.pc = command.args
                     continue
-
             elif command.name == "HALT":
                 break
-
             # conditional halt
             elif command.name == "CHALT":
                 if self.stack[-1] == 1:
                     break
-
             elif command.name == "CMP":
                 x, y = self.stack.pop(), self.stack.pop()
-
                 if x == y:
                     self.stack.append(1)
                 else:
                     self.stack.append(0)
-
             elif command.name in Interpreter.operations:
-                x = self.stack.pop()
-                y = self.stack.pop()
-
+                x, y = self.stack.pop(), self.stack.pop()
                 if command.name == "ADD":
                     self.stack.append(x + y)
                 elif command.name == "SUB":
@@ -113,7 +119,7 @@ class Interpreter(object):
                     if y == 0:
                         pass
                     else :
-                        self.stack.append(int(x // y))
+                        self.stack.append(x // y)
                 elif command.name == "MOD":
                     #error
                     if y == 0:
@@ -123,13 +129,8 @@ class Interpreter(object):
             else:
                 print("Unhandled bytecode: %s" % command)
                 return 1
-
             self.pc += 1
-
         return 0
-
-if __name__ == "__main__":
-    main(sys.argv)
 
 def main(args):
     prog = []
@@ -137,15 +138,18 @@ def main(args):
         program_file = open_file_as_stream(args[1])
         lines = program_file.readall().split("\n")
         program_file.close()
-
         for line in lines:
             line = line.strip()
-
             # some lines are 'commented' with '#'
             if not line.startswith("#"):
                 command = line.split(" ")
-                prog.append(Op(command[0], len(command) > 1 and [command[1]] or None))
-
+                if command[0] == "LOAD" or command[0] == "STORE":
+                    prog.append(StringOp(command[0], command[1]))
+                elif command[0] == "PUSH"  or command[0] == "POP" or \
+                    command[0] == "JMP" or command[0] == "CJMP":
+                    prog.append(IntOp(command[0], command[1]))
+                else:
+                    prog.append(Op(command[0]))
     except IndexError as e:
         print "[ERROR] Need an argument"
     except IOError as e:
@@ -153,5 +157,7 @@ def main(args):
     else:
         interp = Interpreter(prog)
         interp.run()
-
     return 0
+
+if __name__ == "__main__":
+    main(sys.argv)
