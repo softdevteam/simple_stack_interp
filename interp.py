701fb936f649bdce34b307ad67d0d57d7a2c374d
Gabriela Alexandra Moldovan <gabi_250@live.com>
2016-06-08 16:14:09 +0100

    Added opcodes for the command names, replaced string comparisons by int comparisons

--- interp.py
+++ interp.py
@@ -36,8 +36,14 @@ class IntOp(Op):
         Op.__init__(self, name)
         self.args = int(args)
 
+opcodes = {}
+for i, cmd in enumerate("PUSH POP JMP CJMP CMP LOAD STORE HALT CHALT ADD SUB MUL DIV MOD PRINT".split()):
+    globals()[cmd] = i
+    opcodes[cmd] = i
+
 class Interpreter(object):
-    operations = ["ADD", "SUB", "MUL", "DIV", "MOD"]
+
+    operations = [ADD, SUB, MUL, DIV, MOD]
 
     def __init__(self, prog):
         self.prog = prog
@@ -69,38 +75,38 @@ class Interpreter(object):
         while self.pc < len(self.prog):
             jit_driver.jit_merge_point(pc = self.pc, self = self)
             command = self.prog[self.pc]
-            if command.name == "PUSH":
+            if command.name == PUSH:
                 assert isinstance(command, IntOp)
                 self.stack.append(command.args)
-            elif command.name == "POP":
+            elif command.name == POP:
                 assert isinstance(command, Op)
                 self.stack.pop()
-            elif command.name == "PRINT":
+            elif command.name == PRINT:
                 assert isinstance(command, Op)
                 print self.stack[-1]
-            elif command.name == "STORE":
+            elif command.name == STORE:
                 assert isinstance(command, StringOp)
                 self.set_variable(command.args, self.stack.pop())
-            elif command.name == "LOAD":
+            elif command.name == LOAD:
                 assert isinstance(command, StringOp)
                 self.stack.append(self.get_variable(command.args))
-            elif command.name == "JMP":
+            elif command.name == JMP:
                 assert isinstance(command, IntOp)
                 self.pc = command.args
                 continue
-            elif command.name == "CJMP":
+            elif command.name == CJMP:
                 assert isinstance(command, IntOp)
                 x = self.stack.pop()
                 if x == 1:
                     self.pc = command.args
                     continue
-            elif command.name == "HALT":
+            elif command.name == HALT:
                 break
             # conditional halt
-            elif command.name == "CHALT":
+            elif command.name == CHALT:
                 if self.stack[-1] == 1:
                     break
-            elif command.name == "CMP":
+            elif command.name == CMP:
                 x, y = self.stack.pop(), self.stack.pop()
                 if x == y:
                     self.stack.append(1)
@@ -108,19 +114,19 @@ class Interpreter(object):
                     self.stack.append(0)
             elif command.name in Interpreter.operations:
                 x, y = self.stack.pop(), self.stack.pop()
-                if command.name == "ADD":
+                if command.name == ADD:
                     self.stack.append(x + y)
-                elif command.name == "SUB":
+                elif command.name == SUB:
                     self.stack.append(x - y)
-                elif command.name == "MUL":
+                elif command.name == MUL:
                     self.stack.append(x * y)
-                elif command.name == "DIV":
+                elif command.name == DIV:
                     #error
                     if y == 0:
                         pass
                     else :
                         self.stack.append(x // y)
-                elif command.name == "MOD":
+                elif command.name == MOD:
                     #error
                     if y == 0:
                         pass
@@ -141,15 +147,15 @@ def main(args):
         for line in lines:
             line = line.strip()
             # some lines are 'commented' with '#'
-            if not line.startswith("#"):
+            if not line.startswith("#") and line:
                 command = line.split(" ")
                 if command[0] == "LOAD" or command[0] == "STORE":
-                    prog.append(StringOp(command[0], command[1]))
-                elif command[0] == "PUSH"  or command[0] == "POP" or \
+                    prog.append(StringOp(opcodes[command[0]], command[1]))
+                elif command[0] == "PUSH" or command[0] == "POP" or \
                     command[0] == "JMP" or command[0] == "CJMP":
-                    prog.append(IntOp(command[0], command[1]))
+                    prog.append(IntOp(opcodes[command[0]], command[1]))
                 else:
-                    prog.append(Op(command[0]))
+                    prog.append(Op(opcodes[command[0]]))
     except IndexError as e:
         print "[ERROR] Need an argument"
     except IOError as e:
